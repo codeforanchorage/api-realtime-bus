2bcf591625abfd453ada58a5675597e3e74fc066
James Davis <ragingsmurf@gmail.com>
2015-09-06 10:15:08 -0800

    Cleaned up code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-var koa = require("koa"), app = module.exports = koa(), routes =
-require("koa-route"), parse = require("co-body"), monk = require("monk"), wrap =
-require("co-monk"), db = monk("192.168.59.103/Realtime"), oneTable =
-wrap(db.get("one")), request = require('koa-request'),
-parseString = require('xml2js').parseString;
+var koa = require("koa"),
+    app = module.exports = koa(),
+    routes = require("koa-route"),
+    parse = require("co-body"),
+    monk = require("monk"),
+    wrap = require("co-monk"),
+    db = monk("192.168.59.103/Realtime"),
+    oneTable = wrap(db.get("one")),
+    request = require('koa-request'),
+    parseString = require('xml2js').parseString;
 
 //Setup Routing
 app.use(routes.get("/", getDefault));
@@ -10,38 +15,38 @@ app.use(routes.get("/stops", getStopsDepartures));
 
 app.listen("3000");
 
+
+
 //Set default page.
 function * getDefault() {
-
   this.body = "Muni of Anchorage realtime bus api default page.";
   this.status = 200;
-
 };
 
-function * getStopsDepartures() {
+//Fetches all stops & depatures, returns as an Obj array
+function *getStopsDepartures() {
 
     var options = {
         url: 'http://bustracker.muni.org/InfoPoint/XML/stopdepartures.xml',
         headers: { 'User-Agent': 'request' }
-    };
+    },
+    _self = this,
+    response = yield request(options);
 
-    var _self = this;
-
-    var response = yield request(options);
-    //var info = JSON.parse(response.body);
-
-    parseString(response.body, function (err, result) {
-
-      //var jason = JSON.parse(result);
-
-      var last = result.departures.generated[0]._;
-
-      console.log(new Date(last));
-      console.log(last);
-
-      _self.body = "All";
-      _self.status = 200;
-
-    });
-
+    //Check the request
+    if (response.status = 200) {
+      //Parse the XML into an Object.
+      parseString(response.body, function (err, result) {
+        //var jason = JSON.parse(result);
+        //var last = result.departures.generated[0]._;
+        _self.body = JSON.stringify(result);
+        _self.status = 500;
+      });
+    } else {
+        _self.status = 500;
+    }
 };
+
+function log(msg) {
+    console.log(msg);
+}
