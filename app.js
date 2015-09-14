9586bb4a69333791f92c9f1cd0b9e18f7ed5b142
James Davis <ragingsmurf@gmail.com>
2015-09-13 19:05:37 -0800

    check-in

--- app.js
+++ app.js
@@ -15,25 +15,31 @@ module.exports.getDefault = function * getDefault() {
 //Send test feed.
 module.exports.getFeedTest = function * getFeed() {
   //Load test proto instance.
-  //Load test proto instance.
-  //var builder = ProtoBuf.loadProtoFile('./gtfs-realtime.proto');
-  //var root = builder.build('transit_realtime');
-  //var obj = new root.TimeRange();
+  var builder = ProtoBuf.loadProtoFile('./gtfs-realtime.proto');
+  var root = builder.build('transit_realtime');
+  var obj = new root.TimeRange();
 
-  var builder = ProtoBuf.loadProtoFile('./json.proto');
-  var root = builder.build('js');
-  var obj = new root.Component();
+  // var builder = ProtoBuf.loadProtoFile('./json.proto');
+  // var root = builder.build('js');
+  // var obj = new root.Component();
 
   //Set values, pass validation.
-  obj.id = '1';
-  obj.name = 'Muni.org';
-  obj.version = '1';
+  // obj.id = '1';
+  // obj.name = 'Muni.org';
+  // obj.version = '1';
+  obj.start = '1';
+  console.log('Sending Object' + obj);
 
-  //console.log('Object' + obj);
   //console.log('Sending:' + obj.encode());
   //console.log('Sending length: ' + obj.encode().buffer.length);
 
+  var bb = new Buffer(obj.encode().length);
+  var enc = obj.encode();
+  bb.writeInt8(0);
+  //bb.writeVarint32(enc.remaining());
+  bb.append(enc);
+
   //Setup the response
-  this.body = obj.encode();
+  this.body = bb;
   this.status = 200;
 };
--- test/server.js
+++ test/server.js
@@ -1,9 +1,9 @@
 var app = require('./../bin/www.js');
-var request = require('supertest').agent(app.listen());
+var request = require('supertest').agent(app.listen(3001));
 var ProtoBuf = require('protobufjs');
 
-describe('Default Page', function() {
-  it('Fetch the root page.', function(done) {
+describe.skip('Ping pong', function() {
+  it('can fetch the root page of the node server.', function(done) {
     request
       .get('/')
       .expect(200) //Status 200.
@@ -14,58 +14,131 @@ describe('Default Page', function() {
   });
 });
 
-describe('GTFS feed', function() {
-  it('Fetch and decode the GTFS object.', function(done) {
+describe.skip('Webclient', function() {
+  it('can fetch and decode the a .proto object served up by node.', function(done) {
+
+    var http = require('http');
+
+    var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+    var request = require('request');
+
+    var requestSettings = {
+      method: 'GET',
+      url: 'http://localhost:3000/feedtest',
+      encoding: null,
+    };
+
+    //request(requestSettings, function(error, response, body) {
+      //console.log('Response Content-Type: ' + response.get('Content-Type'));
+      //console.log('Response Content-Length: ' + response.get('Content-Length'));
+
+      //console.log(body);
+
+    //  if (!error && response.statusCode == 200) {
+    //    var feed = GtfsRealtimeBindings.TimeRange.decode(body);
+  //      console.log(feed);
+
+        // feed.entity.forEach(function(entity) {
+        //   if (entity.trip_update) {
+        //     console.log(entity.trip_update);
+        //   }
+        // });
+    //  }
+  //  });
 
     //var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
     var transit = ProtoBuf.protoFromFile('gtfs-realtime.proto').build('transit_realtime');
 
-    function binaryParser(res, callback) {
-      res.data = [];
-      res.on('data', function(chunk) {
-        res.data = chunk;
-      });
+    // HTTP GET the binary feed
+    http.get('http://localhost:3000/feedtest', parse);
 
-      res.on('end', function() {
+    // process the feed
+    function parse(res) {
+        // gather the data chunks into a list
+        var data = [];
 
-        var builder = ProtoBuf.loadProtoFile('./json.proto');
-        var root = builder.build('js');
-        var obj = new root.Component();
+        // res.on('data', function(chunk) {
+        //   data.push(chunk);
+        // });
+        //
+        // res.on('end', function() {
+        //   // merge the data to one buffer, since it's in a list
+        //   data = Buffer.concat(data);
+        //
+        //   // create a FeedMessage object by decooding the data with the protobuf object
+        //   var msg = transit.TimeRange.decode(data);
+        //
+        //   // do whatever with the object
+        //   console.log(msg);
+        // });
 
-        root.Component.decode(res.data);
+        res.on('data', function(chunk) {
+          data.push(chunk);
+        }).on('end', function() {
+          //at this point data is an array of Buffers
+          //so we take each octet in each of the buffers
+          //and combine them into one big octet array to pass to a
+          //new buffer instance constructor
+          var buffer = new Buffer(data.reduce(function(prev, current) {
+            return prev.concat(Array.prototype.slice.call(current));
+          }, []));
 
-        //console.log(res.data);
-        // merge the data to one buffer, since it's in a list
-        //data = Buffer.concat(data);
+          console.log(buffer);
 
-        // create a FeedMessage object by decooding the data with the protobuf object
-        //var msg = transit.TimeRange.decode(res.data[0]);
+          //console.log(({}).toString.call(buffer).match(/\s([a-zA-Z]+)/)[1].toLowerCase());
 
-        // do whatever with the object
-        //console.log(msg);
-      });
-    }
-
-    request
-      .get('/feedtest')
-      .expect(200) //Status 200.
-      .parse(binaryParser)
-      .end(function(err, res) {
-        if (err) return done(err);
-        if (!err && res.statusCode == 200) {
-
-          console.log('Response Content-Type: ' + res.get('Content-Type'));
-          console.log('Response Content-Length: ' + res.get('Content-Length'));
-
-          //console.log(res);
-
-          //var rslt = transit.TimeRange.decode(Buffer.concat(res.body));
-
-          //console.log(rslt);
+          var rslt = GtfsRealtimeBindings.TimeRange.decode(buffer);
 
           done();
-        }
-      });
+        });
+      };
+
+    // function binaryParser(res, callback) {
+    //   res.data = [];
+    //   res.on('data', function(chunk) {
+    //     res.data = chunk;
+    //   });
+    //
+    //   res.on('end', function() {
+    //
+    //     var builder = ProtoBuf.loadProtoFile('./json.proto');
+    //     var root = builder.build('js');
+    //     var obj = new root.Component();
+    //
+    //     root.Component.decode(res.data);
+    //
+    //     //console.log(res.data);
+    //     // merge the data to one buffer, since it's in a list
+    //     //data = Buffer.concat(data);
+    //
+    //     // create a FeedMessage object by decooding the data with the protobuf object
+    //     //var msg = transit.TimeRange.decode(res.data[0]);
+    //
+    //     // do whatever with the object
+    //     //console.log(msg);
+    //   });
+    // }
+
+    // request
+    //   .get('/feedtest')
+    //   .expect(200) //Status 200.
+    //   .parse(binaryParser)
+    //   .end(function(err, res) {
+    //     if (err) return done(err);
+    //     if (!err && res.statusCode == 200) {
+    //
+    //       console.log('Response Content-Type: ' + res.get('Content-Type'));
+    //       console.log('Response Content-Length: ' + res.get('Content-Length'));
+    //
+    //       //console.log(res);
+    //
+    //       //var rslt = transit.TimeRange.decode(Buffer.concat(res.body));
+    //
+    //       //console.log(rslt);
+    //
+    //       done();
+    //     }
+    //   });
 
   });
 });
